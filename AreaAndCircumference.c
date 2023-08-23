28cbd7c1018e70094eb33a2bc5a986ebc2ffd7f5
Anonymous140504 <142793463+Anonymous140504@users.noreply.github.com>
2023-08-24 03:53:59 +0530

    Create AreaAndCircumference.c

---
+++ AreaAndCircumference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float r, a ,c;
+    printf("enter radius :\n");
+    scanf("%f", &r);
+    a = 3.14*r*r;
+    c = 2*3.14*r;
+    printf("Area = %f\n circumference = %f\n",a,c);
+    return 0;
+}
