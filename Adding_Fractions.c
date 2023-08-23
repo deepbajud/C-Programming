52946b26439284bd9bae20383ed6661fc2b3b601
Anonymous140504 <142793463+Anonymous140504@users.noreply.github.com>
2023-08-24 03:18:12 +0530

    Create Adding_Fractions.c

---
+++ Adding_Fractions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+
+	int numerator1, numerator2, denominator1, denominator2, 
+num_result, denom_result ;
+
+	printf("Please provide the first numerator:\n");
+	scanf("%d",&numerator1);
+	printf("Please provide the first denominator:\n");
+	scanf("%d",&denominator1);
+ 	printf("Please provide the second numerator:\n");
+        scanf("%d",&numerator2);
+	printf("Please provide the second denominator:\n");
+	scanf("%d",&denominator2);
+
+	if ( denominator1 == denominator2 ) {
+		num_result = numerator1 + numerator2;
+		denom_result = denominator1; 
+	} else {
+		num_result = (numerator1 * denominator2) + (numerator2 * denominator1 );
+		denom_result = denominator1 * denominator2;
+	}
+	printf("The result of %d / %d  + %d / %d  is: %d / %d \n", numerator1,denominator1, numerator2, denominator2,num_result,denom_result);
+	return 0;
+}
+
+
